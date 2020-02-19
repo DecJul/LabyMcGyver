40baecd79b891489db2418641a23d5684b53681c
DecJul <decombejulien@gmail.com>
2020-02-19 14:26:09 +0100

    V1 terminé, hotfix à prévoir

--- LabyMcGyver.py
+++ LabyMcGyver.py
@@ -1,83 +1,85 @@
 # -*- coding: utf8 -*-
+import random
+map = ["M        X     "," XXXXXXX   XXX ","    X   XXX  X ","XXX XX XXX   X ","          XX   ","XXX X   XX XXXX","    X X  X     "," XXXX XX X XXXX","      X  X     ","XXXXXXX XXXX XX","  X   X        "," XX X X XX XXX ","    X X X    X "," XXXXXX XXXXXX ","             XG"]
+gotTube = '_'
+gotAiguille = '_'
+gotEther = '_'
+j = 0
+for i in map:
+    map[j] = list(i)
+    j = j + 1
+
 def initGame():
-    for i de 0 à 14:
-        for j de 0 à 14:
-            map(i, j)=read('map.txt')............................................
-    xItem, yItem = searchFreeSpace()
-    map(xItem, yItem) = E //Ether
-    xItem, yItem = searchFreeSpace()
-    map(xItem, yItem) = T //Tube
-    xItem, yItem = searchFreeSpace()
-    map(xItem, yItem) = A //Aiguille
+    x, y = searchFreeSpace()
+    map[x][y] = 'E' 
+    x, y = searchFreeSpace()
+    map[x][y] = 'T' 
+    x, y = searchFreeSpace()
+    map[x][y] = 'A' 
 
 
 def searchFreeSpace():
     k = 'X'
-    while k != '_':
-        x = random(0-14)
-        y = random(0-14)
-        k = map(x,y)
-    return (x,y)	
+    while k != ' ':
+        x = random.randint(0, 14)
+        y = random.randint(0, 14)
+        k = map[x][y]
+    return x,y
 
 
-def showMap():
-    for i de 0 à 14:
-        for j de 0 à 14:
-            print(map(i,j))
+def showMap(map):
+    for i in map:
+        for j in i:
+            print(j, end='')
+        print('')
     print("Tube=("+gotTube+") Aiguille=("+gotAiguille+") Ether=("+gotEther+").")
 
 
-def move():
-    print("Dans quel direction voulez vous allez?")
-    direction = read()
-    if direction == 'z' && yMcG != 0 && map(xMcG, yMcG+1) != 'X':
-        map(xMcG, yMcG)='_'
-        yMcG++
-        item()
-        map(xMcG, yMcG)='M'
-    elif direction == 'q' && xMcG != 14 && map(xMcG-1, yMcG) != 'X':
-        map(xMcG, yMcG)='_'
-        xMcG--
-        item()
-        map(xMcG, yMcG)='M'
-    elif direction == 's' && yMcG != 14 && map(xMcG, yMcG-1) != 'X':
-        map(xMcG, yMcG)='_'
-        yMcG--
-        item()
-        map(xMcG, yMcG)='M'
-    elif direction == 'd' && xMcG != 0 && map(xMcG+1, yMcG) != 'X':
-        map(xMcG, yMcG)='_'
-        xMcG++
-        item()
-        map(xMcG, yMcG)='M'
+def move(x, y,gameOn,win,gotTube,gotAiguille,gotEther):
+    direction = input("Dans quel direction voulez vous allez? (z,q,s,d)")
+    if direction == 'z' and x != 0 and map[x-1][y] != 'X':
+        map[x][y]=' '
+        x = x - 1
+        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
+        map[x][y]='M'
+    elif direction == 'q' and y != 0 and map[x][y-1] != 'X':
+        map[x][y]=' '
+        y = y-1
+        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
+        map[x][y]='M'
+    elif direction == 's' and x != 14 and map[x+1][y] != 'X':
+        map[x][y]=' '
+        x=x+1
+        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
+        map[x][y]='M'
+    elif direction == 'd' and y != 14 and map[x][y+1] != 'X':
+        map[x][y]=' '
+        y=y+1
+        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
+        map[x][y]='M'
     else:
         print("Commande non autorisée!")
+        x,y=move(x, y,gameOn,win,gotTube,gotAiguille,gotEther)
+    return x,y
 
-
-def item():
-    if map(xMcG, yMcG) == 'E':
+def item(x,y,gameOn,win,gotTube,gotAiguille,gotEther):
+    if map[x][y] == 'E':
         gotEther = 'X'
-    elif map(xMcG, yMcG) == 'A':
+    elif map[x][y] == 'A':
 	    gotAiguille = 'X'
-    elif map(xMcG, yMcG) == 'T':
+    elif map[x][y] == 'T':
         gotTube = 'X'
-    elif xMcG == 14 && yMcG == 14:
+    elif x == 14 and y == 14:
         gameOn = False
-        if gotEther == 'X' && gotAiguille == 'X' && gotTube == 'X':
+        if gotEther == 'X' and gotAiguille == 'X' and gotTube == 'X':
             win = True
 
 
 def main():
-    map = []
-    xMcG, yMcG = 0 , 0
-    gotTube = '_'
-    gotAiguille = '_'
-    gotEther = '_'
     gameOn = True
     win = False
-
+    xMcG, yMcG = 0 , 0
     initGame()
-    
     print("Bienvenue dans McGyver s'échape du labyrinthe infernal!")
     print(" ")
     print("Votre mission: sortir du labyrinthe après avoir neutralisé le garde")
@@ -86,12 +88,12 @@ def main():
     print("M=votre personnage G=Guardien E=Ether T=Tube A=Aiguille")
     print("Déplacement : z,q,s,d")
     print("Bonne chance!")
-    read()
+    input()
     
-    showMap()
+    showMap(map)
     while gameOn:
-        move()
-        showMap()
+        xMcG, yMcG = move(xMcG, yMcG,gameOn,win,gotTube,gotAiguille,gotEther)
+        showMap(map)
 
     print("Game Over!")
     print(" ")
@@ -99,3 +101,5 @@ def main():
         print("Vous avez Gagnez!")
     else:
         print("Vous avez perdu!")
+
+main()
