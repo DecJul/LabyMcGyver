596e052ab6487eaf41b148706b997d2ba047ae44
DecJul <decombejulien@gmail.com>
2020-02-22 22:17:53 +0100

    V1>V2 mise en place des class

--- LabyMcGyver.py
+++ LabyMcGyver.py
@@ -1,105 +1,81 @@
 # -*- coding: utf8 -*-
 import random
-map = ["M        X     "," XXXXXXX   XXX ","    X   XXX  X ","XXX XX XXX   X ","          XX   ","XXX X   XX XXXX","    X X  X     "," XXXX XX X XXXX","      X  X     ","XXXXXXX XXXX XX","  X   X        "," XX X X XX XXX ","    X X X    X "," XXXXXX XXXXXX ","             XG"]
-gotTube = '_'
-gotAiguille = '_'
-gotEther = '_'
-j = 0
-for i in map:
-    map[j] = list(i)
-    j = j + 1
 
-def initGame():
-    x, y = searchFreeSpace()
-    map[x][y] = 'E' 
-    x, y = searchFreeSpace()
-    map[x][y] = 'T' 
-    x, y = searchFreeSpace()
-    map[x][y] = 'A' 
+class Game:
+	def __init__(self):
+		self.on = True
 
+	def over(self):
+		self.on = False
+		
+		
+class Position:
+	def __init__(self, latitude, longitude):
+		self.lat = latitude
+		self.lon = longitude
+		
+	
+class McGyver:
+	def __init__(self):
+		self.pos = Position(1, 1)
+		self.seringue = False
+		
+	def move(self, direction):
+		pass
+		
+	def craft_seringue(self)
+		self.seringue = True
+			
+			
+class Labyrinthe:
 
-def searchFreeSpace():
-    k = 'X'
-    while k != ' ':
-        x = random.randint(0, 14)
-        y = random.randint(0, 14)
-        k = map[x][y]
-    return x,y
+	Lat_Min = 1
+	Lon_min = 1
+	Lat_Max = 15
+	Lon_Max = 15
+	Walls = []
+	Items = []
+	
+	def recencement_wall(cls)
+		open laby.txt
+		for i in lines:
+			for j in i:
+				if j == 'X':
+					position = Position(len(i),j)
+					wall = Wall(position)
+					cls.walls.append(wall)
+	
+	def recencement_item(cls):
+		for i in 2:
+			do = True
+			while do:
+				latitude = random.randint (cls.Lat_Min,cls.Lat_Max)
+				longitude = random.randint (cls.Lon_Min,cls.Lon_Max)	
+				position = Position (latitude, longitute)
+				wall = Wall(position)
+					if wall not in cls.walls:
+						do = False
+			item = Item(name, position)
+			cls.items.append(item)
+			
+			
+class Item:
+	def __init__(self, name, position):
+		self.pos = position
+		self.name = name
+		self.loot = False
+		
+	def loot(self):
+		self.loot = True
 
-
-def showMap(map):
-    for i in map:
-        for j in i:
-            print(j, end='')
-        print('')
-    print("Tube=("+gotTube+") Aiguille=("+gotAiguille+") Ether=("+gotEther+").")
-
-
-def move(x, y,gameOn,win,gotTube,gotAiguille,gotEther):
-    direction = input("Dans quel direction voulez vous allez? (z,q,s,d)")
-    if direction == 'z' and x != 0 and map[x-1][y] != 'X':
-        map[x][y]=' '
-        x = x - 1
-        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
-        map[x][y]='M'
-    elif direction == 'q' and y != 0 and map[x][y-1] != 'X':
-        map[x][y]=' '
-        y = y-1
-        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
-        map[x][y]='M'
-    elif direction == 's' and x != 14 and map[x+1][y] != 'X':
-        map[x][y]=' '
-        x=x+1
-        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
-        map[x][y]='M'
-    elif direction == 'd' and y != 14 and map[x][y+1] != 'X':
-        map[x][y]=' '
-        y=y+1
-        item(x,y,gameOn,win,gotTube,gotAiguille,gotEther)
-        map[x][y]='M'
-    else:
-        print("Commande non autorisée!")
-        x,y=move(x, y,gameOn,win,gotTube,gotAiguille,gotEther)
-    return x,y
-
-def item(x,y,gameOn,win,gotTube,gotAiguille,gotEther):
-    if map[x][y] == 'E':
-        gotEther = 'X'
-    elif map[x][y] == 'A':
-	    gotAiguille = 'X'
-    elif map[x][y] == 'T':
-        gotTube = 'X'
-    elif x == 14 and y == 14:
-        gameOn = False
-        if gotEther == 'X' and gotAiguille == 'X' and gotTube == 'X':
-            win = True
-
+		
+class Wall:
+	def __init__(self, position):
+		self.pos = position
 
 def main():
-    gameOn = True
-    win = False
-    xMcG, yMcG = 0 , 0
-    initGame()
-    print("Bienvenue dans McGyver s'échape du labyrinthe infernal!")
-    print(" ")
-    print("Votre mission: sortir du labyrinthe après avoir neutralisé le garde")
-    print("grace à une seringue fabriqué à partir d'un tube d'une aiguille et de l'Ether")
-    print("que vous aurez précèdemment récupéré.")
-    print("M=votre personnage G=Guardien E=Ether T=Tube A=Aiguille")
-    print("Déplacement : z,q,s,d")
-    print("Bonne chance!")
-    input()
-    
-    showMap(map)
-    while gameOn:
-        xMcG, yMcG = move(xMcG, yMcG,gameOn,win,gotTube,gotAiguille,gotEther)
-        showMap(map)
-
-    print("Game Over!")
-    print(" ")
-    if win:
-        print("Vous avez Gagnez!")
-    else:
-        print("Vous avez perdu!")
+	game = Game()
+	laby = Labyrinthe()
+    joueur = McGyver()
 
 main()
